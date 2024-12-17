fa81459e72e4131e2de964950e9b61375001c714
Abhay Panchal <abhay.techbit@outlook.com>
2024-12-17 14:49:21 +0530

    Added a new endpoint to get all the user.

---
+++ IMS.Application/Features/Auth/Queries/GetAllUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using IMS.Core.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace IMS.Application.Features.Auth.Queries
+{
+    public class GetAllUserCommandHandler : IRequestHandler<GetAllUserCommand, List<ApplicationUser>>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GetAllUserCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<List<ApplicationUser>> Handle(GetAllUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var users = await _userManager.GetUsersInRoleAsync("user");
+                return users.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to retrieve users", ex);
+            }
+        }
+    }
+}
