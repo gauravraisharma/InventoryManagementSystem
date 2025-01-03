e13b9c590478bddb699cb14b6095f1b76436a756
singh1212023 <singh1212023@gmail.com>
2025-01-03 19:57:33 +0530

    Product detail Changes, worked on changes for header,signup, order ssummary, shareProduct- dynamic urls

--- IMS.Shared/RequestDto/orderDTOs/AddOrderDto.cs
+++ IMS.Shared/RequestDto/orderDTOs/AddOrderDto.cs
@@ -20,6 +20,7 @@ namespace IMS.Shared.RequestDto.orderDTOs
         public string Name { get; set; }
         public int Quantity { get; set; }
         public decimal Amount { get; set; }
+        public decimal UnitAmount { get; set; }
     }
     public class CreateStripeCheckoutSessionResponseDto
     {
